452b03d6b4f152f630aa415e8a51aa0806d2fe5b
Ryan Davis <itsryzahd@gmail.com>
2024-10-17 15:57:37 +0000

    feat: added code for menu screen

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#menu screen
+def main_menu():
+    print("Press 1 to play. Press 2 for instructions. Press 3 to exit.")
+
+    while True:
+        try:
+            menu_select = int(input("Press key: \n"))
+            if menu_select == 1:
+                run_game()
+                break
+            elif menu_select == 2:
+                instructions()
+                break
+            elif menu_select == 3:
+                print("""
+                   Exiting game...
+
+                   ...Game closed.
+                """)
+                break
+            else:
+                raise ValueError
+
+        except ValueError:
+            print("""
+            Invalid key pressed. Please choose one of the following:\n
+            Press 1 to play. Press 2 for instructions. Press 3 to exit.
+            """)
+
+
+main_menu()
